c707136f72be3467a7cf175e883687bcebad636c
藤原琢巳 <fujiwaratakumi@fujiwaratakuminoMacBook-Pro.local>
2022-02-17 16:04:41 +0900

    Headerの作成まで

---
+++ Front3/components/article/ArticleDefault.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>記事一覧</h1>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
---
+++ Front3/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <article-default/>
+  </div>
+</template>
+
+<script>
+import ArticleDefault from '../components/article/ArticleDefault.vue'
+
+export default {
+  components: { ArticleDefault },
+  name: 'IndexPage'
+}
+</script>
