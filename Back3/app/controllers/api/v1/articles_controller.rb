6c2a66baeec95c1d0772a0c8fc7e8c97c13c69b5
fujiwaratakumi <fujiwaratakumi@git.email.com>
2022-02-19 19:43:46 +0900

    DB上での削除まで

--- Back3/app/controllers/api/v1/articles_controller.rb
+++ Back3/app/controllers/api/v1/articles_controller.rb
@@ -19,7 +19,7 @@ class Api::V1::ArticlesController < ApplicationController
 
   def destroy
     article = Article.find(params[:id])
-    article.destroy
+    article.destroy!
   end
 
   def update
