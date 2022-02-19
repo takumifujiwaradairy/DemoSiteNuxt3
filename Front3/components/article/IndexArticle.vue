8f5c445b8747d0deea287b2a36f956e13cf0dc24
Your Name <you@example.com>
2022-02-19 17:11:15 +0900

    コンフリクトの修正

---
+++ Front3/components/article/IndexArticle.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>記事の一覧</h1>
+
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+export default {
+  computed: { 
+    
+  },
+  methods: {
+    ...mapActions(['ferchArticles']),
+  },
+  created (){
+    this.ferchArticles()
+  }
+}
+</script>
+
+<style>
+
+</style>
--- Front3/store/index.js
+++ Front3/store/index.js
@@ -14,7 +14,10 @@ const createStore = () =>{
         await axios.post(url, article).then(response => {
           commit('addArticle', response.data)
         })
-      }
+      },
+      async fetchArticles({commit}){
+        await axios.get(url)
+      },
     },
     mutations: {
       addArticle: (state, article) => state.articles.push(article),
