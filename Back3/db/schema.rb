601e614b5bf1fc41caf76071783b84be1444c1e8
fujiwaratakumi <fujiwaratakumi@git.email.com>
2022-02-21 09:53:16 +0900

    記事にユーザーIDを持たせるようにする

--- Back3/db/schema.rb
+++ Back3/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_001425) do
+ActiveRecord::Schema.define(version: 2022_02_21_004604) do
 
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
